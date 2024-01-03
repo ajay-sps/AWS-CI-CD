4dd3682495f904199c19399702c08dd72de09e85
ajay-sps <ajay_sihag@softprodigy.com>
2024-01-03 15:23:04 +0530

    database changed

--- cicd/settings.py
+++ cicd/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'cicd.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_database',
+        'USER': 'admin',
+        'PASSWORD': 'admin123',
+        'HOST': 'django-databasee.cxqju0syck9r.ap-south-1.rds.amazonaws.com',
+        'PORT': 3306,
     }
 }
 
